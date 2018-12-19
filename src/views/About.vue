06615ad64624de951a04262b6c6b16fecc5fcf90
Zuks <azuks1@bloomberg.com>
2018-12-18 22:29:22 -0500

    added a bit of formatting and about page

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,11 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <h2>NYC Building Data Visualizer</h2>
+    <p>This site aims to ease uncertainty when moving to a new neighborhood in NYC.
+      It has 2 parts, proving a way to visualize construction and provide forecasts for pricing.</p>
+      <p>
+        The construction visualization uses data from NYC open data and a cloudsearch index to provide faceting and geo-spatial searching. New data is indexed at the end of the day.
+      </p>
+      <p>The price forecasting for rent and sales is done on Street Easy data with the built in DeepAR model in Sagemaker.</p>
   </div>
 </template>
